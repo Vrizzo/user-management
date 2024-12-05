e1521e21b6231ee11bc35740b6b45b396fc75865
Vittorio Rizzo <rizzo.vittorio@gmail.com>
2024-12-05 21:19:13 +0100

    OAuth2.0 server empty basic module

--- user.oauth.2.0/src/main/java/com/mybank/auth/services/UserService.java
+++ user.oauth.2.0/src/main/java/com/mybank/auth/services/UserService.java
@@ -2,14 +2,10 @@ package com.mybank.auth.services;
 
 import com.mybank.auth.repositories.UserRepository;
 import com.mybank.user.core.models.User;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
